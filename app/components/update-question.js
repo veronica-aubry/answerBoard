e95d80c9a82c153644d75d448491595605f996cb
Veronica Alley <veronica.aubry.alley@gmail.com>
2016-04-01 09:17:16 -0700

    add functioning update and delete question methods

---
+++ app/components/update-question.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  updateQuestionForm: false,
+  actions: {
+    updateQuestionForm() {
+      this.set('updateQuestionForm', true);
+    },
+    update(question) {
+      var params = {
+        title: this.get('title'),
+        text: this.get('text'),
+      };
+      this.set('updateQuestionForm', false);
+      this.sendAction('update', question, params);
+    }
+  }
+});
