74b57da67f763608069cf9121b55d16b4d3c8bb8
Veronica Alley <veronica.aubry.alley@gmail.com>
2016-04-01 13:48:18 -0700

    add working comment display

--- app/models/answer.js
+++ app/models/answer.js
@@ -2,7 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   author: DS.attr(),
-  title: DS.attr(),
-  text: DS.attr(),
-  answers: DS.hasMany('answer', {async: true})
+  body: DS.attr(),
+  question: DS.belongsTo('question', {async: true})
 });
--- app/routes/question.js
+++ app/routes/question.js
@@ -2,10 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params){
-    return this.store.findRecord('question', params.question_id);
+    return Ember.RSVP.hash({
+      question: this.store.findRecord('question', params.question_id),
+      answers: this.store.findAll('answer')
+    });
   },
   actions: {
    update(question, params) {
+     console.log(question);
      Object.keys(params).forEach(function(key) {
        if(params[key]!==undefined) {
          question.set(key,params[key]);
@@ -17,6 +21,15 @@ export default Ember.Route.extend({
    destroyQuestion(question) {
      question.destroyRecord();
      this.transitionTo('index');
+   },
+   saveAnswer(params) {
+     var newAnswer = this.store.createRecord('answer', params)
+     var question = params.question;
+     question.get('answers').addObject(newAnswer)
+     newAnswer.save().then(function(){
+       return question.save();
+     });
+     this.transitionTo('question', params.question);
    }
  }
 });
